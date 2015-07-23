9d49f66fcb57015795af571a9f0b32201d04ad50
priyankaDhingra <priyanka@Priyankas-MBP.home>
2015-07-22 23:36:15 -0400

    ll changed

--- spatial/LinkedList.c
+++ spatial/LinkedList.c
@@ -9,8 +9,10 @@
 #include <stdlib.h>
 #include <time.h>
 #include "metadata.c"
+void * HEAD_BASE = 0;
+void * HEAD_BOUND = 1;
 struct freeptrs {
-	void* data;
+	int data;
 	struct freeptrs *next;
 }*head;
 void try_going_beyond(void *p) {
@@ -18,102 +20,98 @@ void try_going_beyond(void *p) {
 	srand(time(NULL));
 	int num = rand() / 1000;
 	printf("num %d=", num);
-	struct metadata_entry me = metadata_load(&head);
-	if (spatial_check(p, me.base, me.bound, num) == 1) {
-		printf("not violated");
 		p = p + num;
-	}else{
-		printf("----------------------violated\n");
-	}
 	printf("display p->data=%p\n", ((struct freeptrs*) p)->next);
 }
-void insertel(struct freeptrs *fp) {
-	struct freeptrs *temp;
-	temp = head;
+
+void insertel(struct freeptrs *fp, void *base, void *bound) {
+
 	if (head == NULL) {
+		//copy pointer
 		head = fp;
+		HEAD_BASE = base;
+		HEAD_BOUND = bound;
 	} else {
-		while (temp->next != NULL) {
+		struct freeptrs *temp;
+		//copy pointer
+		temp = head;
+		struct metadata_entry *tmp_me;
+		tmp_me->base = HEAD_BASE;
+		tmp_me->bound = HEAD_BOUND;
+
+		while ((spatial_check(&(temp),tmp_me->base,tmp_me->bound))&&(temp->next != NULL)) {
+			//copying
+			//check
+
 			temp = temp->next;
+			tmp_me->base = temp->next;
+			tmp_me->bound = (temp->next) + sizeof(struct freeptrs);
 		}
+		//copy pointer
+		//check
 		temp->next = fp;
+		//check
+		tmp_me->base = fp->next;
+		tmp_me->bound = (fp->next) + sizeof(struct freeptrs);
 	}
 }
-void removel(int dt) {
 
-	struct freeptrs *temp;
-	temp = head;
-
-	//if head node
-	if ((int) temp->data == dt) {
-		struct freeptrs *del;
-		del = temp;
-		head = temp->next;
-		free(del);
-	} else {
-		while (temp->next != NULL) {
-			if (((int) temp->next->data) == dt) {
-				struct freeptrs *del;
-				del = temp->next;
-				temp->next = temp->next->next;
-				free(del);
-				break;
-			}
-
-			temp = temp->next;
-		}
-
-	}
-}
 void printall() {
 	struct freeptrs *temp;
+	//copy pointer
 	temp = head;
-	try_going_beyond(temp);
+	struct metadata_entry *tmp_me;
+	tmp_me->base = HEAD_BASE;
+	tmp_me->bound = HEAD_BOUND;
+	try_going_beyond(temp,tmp_me->base,tmp_me->bound);
 	if (head == NULL) {
 		printf("List is empty");
 	} else {
 		while (temp != NULL) {
-			printf("value = %d\n", (int) (temp->data));
+			//check
+			printf("value = %d\n", (temp->data));
+			//indexing/copying
 			temp = temp->next;
 		}
 	}
 }
 
 int main() {
-	//struct freeptrs *xp;
-	//xp->data=121;
-init();
+	init();
 	//pointer allocation
-	struct freeptrs *fp;
+	struct freeptrs *fp, *fp1, *fp2, *fp3, *fp4;
 	fp = (struct freeptrs*) malloc(sizeof(struct freeptrs));
-	fp->data = (int*) 23;
+	fp->data = 23;
 	fp->next = NULL;
-	insertel(fp);
-//	printf("xpnext:=%p", xp->next->data);
-	fp = (struct freeptrs*) malloc(sizeof(struct freeptrs));
+	metadata_store(&fp, fp, fp + sizeof(struct freeptrs));
+	insertel(fp, fp, fp + sizeof(fp));
 
-	fp->data = (int*) 22;
-	fp->next = NULL;
-	insertel(fp);
+	fp1 = (struct freeptrs*) malloc(sizeof(struct freeptrs));
 
-	fp = (struct freeptrs*) malloc(sizeof(struct freeptrs));
-	fp->data = (int*) 212;
-	fp->next = NULL;
-	insertel(fp);
+	fp1->data = 22;
+	fp1->next = NULL;
+	metadata_store(&fp1, fp1, fp1 + sizeof(fp1));
+	insertel(fp1, fp1, fp1 + sizeof(fp));
 
-	fp = (struct freeptrs*) malloc(sizeof(struct freeptrs));
-	fp->data = (int*) 23123;
-	fp->next = NULL;
-	insertel(fp);
+	fp2 = (struct freeptrs*) malloc(sizeof(struct freeptrs));
+	fp2->data = 212;
+	fp2->next = NULL;
+	metadata_store(&fp2, fp2, fp2 + sizeof(fp2));
+	insertel(fp2, fp2, fp2 + sizeof(fp2));
 
-	fp = (struct freeptrs*) malloc(sizeof(struct freeptrs));
-	fp->data = (int*) 2123;
-	fp->next = NULL;
-	insertel(fp);
-	printf("----------------------head+5=%p\n",head+5);
-	metadata_store(&head, head,head+5 );
+	fp3 = (struct freeptrs*) malloc(sizeof(struct freeptrs));
+	fp3->data = 23123;
+	fp3->next = NULL;
+	metadata_store(&fp3, fp3, fp3 + sizeof(fp3));
+	insertel(fp3, fp3, fp3 + sizeof(fp3));
+
+	fp4 = (struct freeptrs*) malloc(sizeof(struct freeptrs));
+	fp4->data = 2123;
+	fp4->next = NULL;
+	metadata_store(&fp4, fp4, fp4 + sizeof(fp4));
+	insertel(fp4, fp4, fp4 + sizeof(fp4));
+
 	int dt = 2123;
-	removel(dt);
 
 	printall();
 
